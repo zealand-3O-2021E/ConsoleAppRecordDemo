ebfd0232158ac6544df0fb88a127552a21d779b7
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2021-09-16 13:28:11 +0200

    Add project files.

---
+++ ConsoleAppRecordDemo/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppRecordDemo
+{
+    public record Teacher(string firstname, string lastname, string grade) :
+        Person( firstname, lastname);
+
+    public  record Person(string firstname, string lastname) {
+
+        public int test { get; set; }
+
+    };
+}
---
+++ ConsoleAppRecordDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleAppRecordDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Car car = new();
+            var car1 = new Car() {price = 20000, Licenceplate = "ABCEDE" };
+
+            Person p1 = new("Martin", "K");
+            Person p2 = new("Martin", "K");
+            if (p1==p2)
+                Console.WriteLine("are equal");
+            else
+                Console.WriteLine("are not equal");
+
+            Person p3 = p1 with { firstname = "Søren" };
+
+            Console.WriteLine(p3);
+
+
+            //Person p2 = new{firstname="Martin",lastname="K"}
+
+            Console.WriteLine(p1);
+
+            Console.WriteLine(car1);
+
+            //car.price = 10000;
+
+
+
+
+        }
+    }
+}
